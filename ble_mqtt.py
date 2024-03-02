4e7da3caffb10e581d4ed0c04309d5b7628911bc
MattiaCotardo <mattia.cotardo@studenti.unisalento.it>
2024-03-02 15:44:28 +0100

    aggiunto passaggio ip come argomento e gestione spegnimento cuffia

--- ble_mqtt.py
+++ ble_mqtt.py
@@ -2,16 +2,28 @@
 from bluepy.btle import Scanner, DefaultDelegate
 import requests
 from getmac import get_mac_address as gma
+import sys
+
+''' Leggi gli argomenti dalla linea di comando '''
+args = sys.argv
+
+if len(args) < 2:
+    print("Usage: python ble_mqtt.py <url>")
+    exit()
+
+
+backend_ip = args[1]
 
 '''ID HEADPHONES'''
 
 idHeadphones = gma()
 
 
+
 '''GET di tutti i macchinari'''
 
 # Esegui la richiesta GET
-response = requests.get("http://192.168.1.108:8080/machinery/getAll")
+response = requests.get("http://"+backend_ip+":8080/machinery/getAll")
 
 # Controlla lo stato della risposta
 if response.status_code == 200:
@@ -33,7 +45,7 @@ machinery_flag = {}
 
 for machinery in machineries:
 
-    response = requests.get("http://192.168.1.108:8080/beacon/find/" + machinery["mserial"])
+    response = requests.get("http://"+backend_ip+":8080/beacon/find/" + machinery["mserial"])
 
     if response.status_code == 200:
 
@@ -68,7 +80,7 @@ class ScanDelegate(DefaultDelegate):
                     if dev.rssi > -60 and machinery_flag[mserial] == False:
 
                         # Esegui la richiesta GET
-                        response = requests.get("http://192.168.1.108:8080/machinery/find/machinery/" + mserial)
+                        response = requests.get("http://"+backend_ip+":8080/machinery/find/machinery/" + mserial)
 
                         # Controlla lo stato della risposta
                         if response.status_code == 200:
@@ -88,7 +100,7 @@ class ScanDelegate(DefaultDelegate):
                             "idBranch": m["idBranch"]
                         }
                         headers = {'Content-Type': 'application/json'}
-                        r = requests.post("http://192.168.1.108:8080/nearbyHeadphones/add", json=payload, headers=headers)
+                        r = requests.post("http://"+backend_ip+":8080/nearbyHeadphones/add", json=payload, headers=headers)
 
                         if r.status_code == 200:
                             machinery_flag[mserial] = True
@@ -101,7 +113,7 @@ class ScanDelegate(DefaultDelegate):
                             "mserial": mserial
                         }
                         headers = {'Content-Type': 'application/json'}
-                        r = requests.delete("http://192.168.1.108:8080/nearbyHeadphones/delete", json=payload, headers=headers)
+                        r = requests.delete("http://"+backend_ip+":8080/nearbyHeadphones/delete", json=payload, headers=headers)
 
                         if r.status_code == 200:
                             machinery_flag[mserial] = False
@@ -116,19 +128,22 @@ def scan_ble_devices():
 
     print("Scanning for BLE devices... Press Ctrl+C to stop.")
 
-    try:
-        while True:
-            devices = scanner.scan(0.5)  # Scans for 0.5 seconds
-    except KeyboardInterrupt:
-        print("Scan stopped.")
-
+    while True:
+        devices = scanner.scan(0.5)  # Scans for 0.5 seconds
+    
 
 # Attesa infinita per mantenere il programma in esecuzione
 try:
     while True:
     	scan_ble_devices()
 except KeyboardInterrupt:
-    print("Connessione al broker MQTT chiusa.")
+    r = requests.delete("http://"+backend_ip+":8080/nearbyHeadphones/delete/"+idHeadphones)
+
+    if r.status_code == 200:
+        print("Disconnessione della cuffia riuscita")
+    else:
+        print("Errore nella richiesta:", r.status_code)
+
 
 
 
